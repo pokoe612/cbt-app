42059cfe2f0898b5162f6a037d7754b8955e9974
pokoe612 <pokoe612@gmail.com>
2026-03-16 03:56:11 +0700

    Add AdminReceiver to manage device admin events
    Implement device admin receiver to handle enabling and disabling events.

--- app/src/main/java/sch/id/smkdukep/cbt/AdminReceiver.kt
+++ app/src/main/java/sch/id/smkdukep/cbt/AdminReceiver.kt
@@ -1,1 +1,17 @@
+package sch.id.smkdukep.cbt
 
+import android.app.admin.DeviceAdminReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class AdminReceiver : DeviceAdminReceiver() {
+    
+    override fun onEnabled(context: Context, intent: Intent) {
+        Toast.makeText(context, "Mode Ujian Aktif", Toast.LENGTH_SHORT).show()
+    }
+    
+    override fun onDisabled(context: Context, intent: Intent) {
+        Toast.makeText(context, "Mode Ujian Dinonaktifkan", Toast.LENGTH_SHORT).show()
+    }
+}
