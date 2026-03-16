5197288e5f23e5792a7a159bc3469515c18ca204
pokoe612 <pokoe612@gmail.com>
2026-03-16 22:54:01 +0700

    Implement toast notifications in AdminReceiver
    Added toast messages for enabling and disabling the admin mode.

--- app/src/main/java/sch/id/smkdukep/cbt/AdminReceiver.kt
+++ app/src/main/java/sch/id/smkdukep/cbt/AdminReceiver.kt
@@ -1,5 +1,20 @@
 package sch.id.smkdukep.cbt
 
 import android.app.admin.DeviceAdminReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
 
-class AdminReceiver : DeviceAdminReceiver()
+class AdminReceiver : DeviceAdminReceiver() {
+
+    override fun onEnabled(context: Context, intent: Intent) {
+        super.onEnabled(context, intent)
+        Toast.makeText(context, "Mode Ujian Aktif", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onDisabled(context: Context, intent: Intent) {
+        super.onDisabled(context, intent)
+        Toast.makeText(context, "Mode Ujian Dinonaktifkan", Toast.LENGTH_LONG).show()
+    }
+
+}
